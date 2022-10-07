4a9da5fe06aec0f2eec3134725d79d1ea6350585
MuhFadila <muhammad.fadila.ft21@mail.umy.ac.id>
2022-10-07 19:14:07 +0700

    Menerima angka untuk menentukan banyaknya data yang di simpan pada array

--- Program.cs
+++ Program.cs
@@ -10,7 +10,27 @@ namespace BubbleSortC__2_
         // Deklarasi varibel int untuk menyimpan banyaknya data pada array
         private int n;
 
-        //
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang di simpan pada array
+            while (true)
+            {
+                Console.Write("Masukan elemen pada array: );
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _")
+            Console.WriteLine("Masukan elemen array");
+            Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _")
+
+
+        }
        
     }
 }
